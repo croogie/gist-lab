dca4c5aa5bca3a3afd420ca1d70fe0d1aa30062e
Michal Switon <switonmichal@gmail.com>
2016-06-07 19:12:23 +0200

    Basic styling for three column layout

--- client/modules/core/components/main_layout.jsx
+++ client/modules/core/components/main_layout.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import style from './main_layout.scss';
 
 const noContent = () => null;
 
 const Layout = ({content = noContent}) => (
-  <div>{content()}</div>
+  <div className={style.container}>
+    <div className={style.menu}>menu</div>
+    <div className={style.list}>list</div>
+    <div className={style.content}>{content()}</div>
+  </div>
 );
 
 export default Layout;
